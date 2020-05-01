aab37c5e6763809ece1bd54839653e7c7c06d456
NewBugger <contacts@sometimesnaive.org>
2020-05-01 15:43:04 +0800

    [announce] update copyright.

--- app/src/main/java/io/github/newbugger/android/screenshot/ScreenshotActivity.kt
+++ app/src/main/java/io/github/newbugger/android/screenshot/ScreenshotActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020.
+ * Copyright (c) 2018-2020 : NewBugger (https://github.com/NewBugger)
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
